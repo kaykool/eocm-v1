0f59afb77e795531d98753cb689bb1895377723f
kangspbu <seljaworks@gmail.com>
2025-11-28 22:45:22 +0700

    add hero image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <div className="main-container min-h-52">TEST</div>;
+  return (
+    <div className="main-container ">
+      <div className="relative aspect-3/1 w-full">
+        <Image src="/featured.png" fill alt="hero image" />
+      </div>
+    </div>
+  );
 }
